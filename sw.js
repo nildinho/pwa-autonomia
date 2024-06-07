93c5af1ede11a01b1d2d8bd6a8ee504bb49a4270
nildinho <nildinho.anahi@hotmail.com>
2024-06-07 16:39:28 -0300

    autonomia

--- sw.js
+++ sw.js
@@ -8,21 +8,28 @@ const urlsToCache = [
   '/volante.png'
 ];
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('install', function (event) {
+  // Instalação do Service Worker e armazenamento dos arquivos no cache
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
+        cacheNames.filter(function (cacheName) {
           return cacheName !== CACHE_NAME;
-        }).map(function(cacheName) {
+        }).map(function (cacheName) {
           console.log('Excluindo cache:', cacheName); // Adiciona um log para registrar o nome do cache sendo excluído
           return caches.delete(cacheName);
         })
       );
-    }).then(function() {
-      console.log('Caches excluídos com sucesso!'); // Adiciona um log para indicar que todos os caches foram excluídos
-    }).catch(function(error) {
-      console.error('Erro ao excluir caches:', error); // Adiciona um log para registrar erros durante a exclusão de caches
     })
   );
 });
