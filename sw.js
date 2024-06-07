6852229f5e20acec410d7a323a9740ebbf10d5cf
nildinho <nildinho.anahi@hotmail.com>
2024-06-07 16:45:23 -0300

    autonomia

--- sw.js
+++ sw.js
@@ -8,28 +8,60 @@ const urlsToCache = [
   '/volante.png'
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
   // Instalação do Service Worker e armazenamento dos arquivos no cache
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function (cache) {
+      .then(function(cache) {
         console.log('Cache aberto');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', function(event) {
+  // Limpa caches antigos durante a ativação do Service Worker
   event.waitUntil(
-    caches.keys().then(function (cacheNames) {
+    caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheNames.filter(function (cacheName) {
+        cacheNames.filter(function(cacheName) {
           return cacheName !== CACHE_NAME;
-        }).map(function (cacheName) {
-          console.log('Excluindo cache:', cacheName); // Adiciona um log para registrar o nome do cache sendo excluído
+        }).map(function(cacheName) {
+          console.log('Excluindo cache:', cacheName);
           return caches.delete(cacheName);
         })
       );
     })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - retorna a resposta do cache
+        if (response) {
+          return response;
+        }
+
+        // Não há cache, faz uma solicitação de rede
+        return fetch(event.request)
+          .then(function(response) {
+            // Verifica se a resposta é válida
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clona a resposta para armazenar em cache
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
   );
 });
