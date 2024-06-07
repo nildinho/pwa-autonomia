ed781ec1e181633c15b0988aea37aff405bbc534
nildinho <nildinho.anahi@hotmail.com>
2024-06-07 16:12:36 -0300

    autonomia

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-
 const CACHE_NAME = 'calculadora-autonomia-cache-v1';
 const urlsToCache = [
   '/',
@@ -22,36 +21,5 @@ self.addEventListener('activate', function(event) {
       );
     })
   );
-});self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-  
-          // Clone the request (to use it once for fetching and once for caching)
-          var fetchRequest = event.request.clone();
-  
-          return fetch(fetchRequest)
-            .then(function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-  
-              // Clone the response (to use it once for returning and once for caching)
-              var responseToCache = response.clone();
-  
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-  
-              return response;
-            });
-        })
-    );
-  });
-  
+});
+
