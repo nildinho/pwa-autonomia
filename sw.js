1bd4787ce80ecd2e19cc3ee10fa5793973aa0a96
nildinho <nildinho.anahi@hotmail.com>
2024-06-07 16:22:04 -0300

    autonomia

--- sw.js
+++ sw.js
@@ -7,19 +7,30 @@ const urlsToCache = [
   '/manifest.json',
   '/volante.png'
 ];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cachePWA)
+      .then((cache) => {
+        return cache.addAll(urlsToCache)
+      })
+  )
+})
 
 self.addEventListener('activate', function(event) {
-  // Ativa o novo service worker e remove os antigos
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.filter(function(cacheName) {
           return cacheName !== CACHE_NAME;
         }).map(function(cacheName) {
+          console.log('Excluindo cache:', cacheName); // Adiciona um log para registrar o nome do cache sendo excluído
           return caches.delete(cacheName);
         })
       );
+    }).then(function() {
+      console.log('Caches excluídos com sucesso!'); // Adiciona um log para indicar que todos os caches foram excluídos
+    }).catch(function(error) {
+      console.error('Erro ao excluir caches:', error); // Adiciona um log para registrar erros durante a exclusão de caches
     })
   );
 });
-
